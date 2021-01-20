0138f400ceb591da22c810b55414fc6287f1a2e0
Dennis Cosgrove <cosgroved@wustl.edu>
2021-01-20 14:04:31 -0600

    initial commit after copy from wustl-cse131-sp20

---
+++ src/lines/InteractiveLines.java
@@ -0,0 +1,44 @@
+package lines;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class InteractiveLines {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		while (true) {
+			//
+			// Wait for the mousebutton to be pressed
+			//
+			while (!StdDraw.isMousePressed()) {
+				// wait, do nothing
+			}
+			//
+			// Capture the mouse's location is now that is has been pressed
+			//
+			double x1 = StdDraw.mouseX();
+			double y1 = StdDraw.mouseY();
+			//
+			// Now wait for the button to be released
+			//
+			while (StdDraw.isMousePressed()) {
+				// wait, do nothing
+			}
+			//
+			// Capture the mouse's location now that it has been released
+			//
+			double x2 = StdDraw.mouseX();
+			double y2 = StdDraw.mouseY();
+			
+			//
+			// Run the student's code to draw the line
+			//
+			Lines.drawLine(x1, y1, x2, y2);
+
+		}
+
+	}
+
+}
---
+++ src/memorymatching/MemoryMatch.java
@@ -0,0 +1,42 @@
+package memorymatching;
+
+import java.awt.Color;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+
+public class MemoryMatch {
+
+	public static Color[][] genBoard(){
+		// FIXME
+		return null;
+	}
+
+	// You may add more parameters if desired
+	public static void drawBoard(Color[][] board){
+		// FIXME
+	}
+
+
+        // You may add more parameters if desired
+	public static boolean isOver(Color[][] board){
+		return false;
+	}
+
+        // You may change the return type to an int[] if desired. 
+	public static double[] click(){
+		// FIXME
+		return new double[] { 0, 0 };
+	}
+	
+	public static void playGame(){
+		// FIXME
+	}
+
+
+
+	public static void main(String[] args) {
+		playGame();
+	}
+
+}
