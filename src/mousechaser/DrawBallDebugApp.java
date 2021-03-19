b86d47efbb28549cb952a4b5138b62c956b8d1f9
Dennis Cosgrove <cosgroved@wustl.edu>
2021-03-19 14:43:22 -0500

    updated chase the mouse draw ball debug app

--- src/mousechaser/DrawBallDebugApp.java
+++ src/mousechaser/DrawBallDebugApp.java
@@ -5,26 +5,43 @@ import javax.swing.SwingUtilities;
 import edu.princeton.cs.introcs.StdDraw;
 import mousechaser.debug.DrawBallCase;
 import mousechaser.debug.DrawBallCases;
-import support.cse131.Crosshairs;
 import support.cse131.DialogBoxes;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class DrawBallDebugApp {
-	private static void drawCrosshairs(DrawBallCase drawBallCase) {
-		StdDraw.setPenColor(0, 109, 219);
-		Crosshairs.drawCrosshairs(drawBallCase.location(), drawBallCase.radius() * 2);
+	private static void drawCrosshair(double x, double y, double halfRadius) {
+		StdDraw.line(x, y - halfRadius, x, y + halfRadius);
+		StdDraw.line(x - halfRadius, y, x + halfRadius, y);
+	}
+
+	private static void draw(DrawBallCase drawBallCase) {
+		double prevPenRadius = StdDraw.getPenRadius();
+		StdDraw.setPenRadius();
+		double defaultPenRadius = StdDraw.getPenRadius();
+		StdDraw.setPenColor(144, 0, 0);
+		StdDraw.setPenRadius(defaultPenRadius * 4);
+		StdDraw.circle(drawBallCase.x(), drawBallCase.y(), drawBallCase.radius());
+		StdDraw.setPenColor(0, 0, 0);
+		drawBallCase.draw();
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.setPenRadius();
+		StdDraw.circle(drawBallCase.x(), drawBallCase.y(), drawBallCase.radius());
+		StdDraw.setPenColor(144, 0, 0);
+		drawCrosshair(drawBallCase.x(), drawBallCase.y(), drawBallCase.radius() * 2);
 		StdDraw.show();
+		StdDraw.setPenRadius(prevPenRadius);
 	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			for (DrawBallCase drawCase : DrawBallCases.getCases()) {
 				StdDraw.clear();
-				drawCase.draw();
-				drawCrosshairs(drawCase);
-				DialogBoxes.askUser(drawCase.getText());
+				draw(drawCase);
+				if (!DialogBoxes.askUser(drawCase.getText())) {
+					break;
+				}
 			}
 			System.exit(0);
 		});
--- support_src/mousechaser/debug/DrawBallCase.java
+++ support_src/mousechaser/debug/DrawBallCase.java
@@ -10,16 +10,22 @@ import support.cse131.DrawCase;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class DrawBallCase implements DrawCase {
-	private final double[] location;
+	private final double x;
+	private final double y;
 	private final double radius;
 
 	public DrawBallCase(double x, double y, double radius) {
-		this.location = new double[] { x, y };
+		this.x = x;
+		this.y = y;
 		this.radius = radius;
 	}
 
-	public double[] location() {
-		return location;
+	public double x() {
+		return x;
+	}
+
+	public double y() {
+		return y;
 	}
 
 	public double radius() {
@@ -29,14 +35,14 @@ public class DrawBallCase implements DrawCase {
 	@Override
 	public String getText() {
 		return String.format(
-				"<html>Does the screen show a single black dot at (%s, %s) of radius: %s?<br/>Note: we have added crosshairs at this location.</html>",
-				location[0], location[1], radius);
+				"<html>Does the screen show a single black circle at (%s, %s) of radius: %s?<br/><br/>Note: we have outlined where the circle should be in yellow and red<br/>and added crosshairs at this location.</html>",
+				x, y, radius);
 	}
 
 	@Override
 	public void draw() {
-		StdDraw.setPenColor(0,0,0);
-		ChaseTheMouse.drawBall(location, radius);
+		StdDraw.setPenColor(0, 0, 0);
+		ChaseTheMouse.drawBall(new double[] { x, y }, radius);
 	}
 
 	@Override
